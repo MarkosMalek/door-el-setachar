50e90c0094e3f911bd7527aaa4cf482b0c11d54e
MarkosMalek <https://github.com/MarkosMalek>
2024-12-24 02:36:03 +0200

    mainly adding three methodes to the user model while only using the .pre hook to hash the password for now every thing else in the user controller is hard coded

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,65 @@
 const asyncHandler = require("express-async-handler");
-//const user=require("../controllers/user.models.js")
+const user=require("../models/user.models.js")
+const bcrypt =require("bcrypt");
 
+
+//register a new user
+const registerUser = asyncHandler(async (req,res)=>{
+    const {name ,email,password}=req.body
+
+    //some validation
+    if(!name || !email || !password){
+        res.send("please enter all fields");
+    }
+    const existingUser = await user.findOne({$or:[{name:name},{email:email}]})
+        if(existingUser==null){
+            const newUser = await user.create({name,email,password});
+            res.send(newUser)
+        }else  res.send("user already exist")
+    
+    // JWT access and refresh tokens
+
+    
+    
+})
+
+//log in
+const logIn = asyncHandler(async (req,res)=>{
+        const {name,email,password}=req.body
+        if(!name || !email || !password){
+            res.send("please enter all fields");
+        }
+        const existingUser = await user.findOne({name:name,email:email})
+        if(existingUser==null){
+            res.send("no user with this name or email,please register as a new user first")
+        }else{
+            const isPasswordCorrect = await bcrypt.compare(password,existingUser.password);
+            if(isPasswordCorrect){
+                res.send(`password is correct wellcome : ${name}`)
+            }else res.send("password is incorrect")
+        }
+        
+
+
+})
+//log out
 
 
 //get all users
 
-const userController = asyncHandler(async (req,res,next)=>{
-    res.send("hello we are at users/all");
+const userController = asyncHandler(async (req,res)=>{
+    const allUsers = await user.find({})
+    res.send(allUsers)
+
 });
 
-module.exports= {userController}
+
+//get a single user
+
+
+//delete a user
+
+//update a user
+
+
+module.exports= {userController,registerUser,logIn}
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,10 +1,20 @@
 const express= require("express");
 const router= express.Router();
 
-const {userController}= require("../controllers/user.controller");
+const { userController,
+        registerUser,
+        logIn
+} = require("../controllers/user.controller");
 
 
 //home page
 router.get("/All",userController);
+
+//register a new user 
+
+router.post("/register",registerUser);
+
+//log-in
+router.post("/login",logIn)
 
 module.exports=router;
